77c2c09f2b2424953a03fb4b682b646366a33779
LeeGeonwoo22 <kwoo.chris@gmail.com>
2022-03-10 11:58:02 +0900

    노마드 코더 바닐라Js 첫 커밋

---
+++ studyScript/andOr.js
@@ -0,0 +1,5 @@
+// 자바스크립트는 작은 괄호에서 시작해서 큰괄호로 넘어간다. 
+
+const or = true  || false || false 
+
+console.log(or) // true
---
+++ studyScript/browser.js
@@ -0,0 +1,12 @@
+// title 바꾸기
+function changeTitle(a){
+    document.title =(a)
+    console.log(document.title)
+}
+
+// title 확인
+function title(){
+    console.log(document.title)
+}
+
+
