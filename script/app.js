4343c3b7ec9d5219425ff0b1e644a4053435f787
LeeGeonwoo22 <kwoo.chris@gmail.com>
2022-03-11 17:39:08 +0900

    login 만들기 -2 form 이벤트

--- script/app.js
+++ script/app.js
@@ -1,23 +1,23 @@
 const loginForm = document.querySelector("#login-form");
-const loginInput = loginForm.querySelector("input");
-const loginButton = loginForm.querySelector("button");
+const loginInput = loginForm.querySelector("#login-form input");
 
-
-function handleLoginButton(){
+function handleLoginButton(e){
+    // 어떤 이벤트의 기본행동이든지 발생하지 않도록 방지함
+    // 기본적으로 페이지를 새로고침하도록 되어있지만 그걸 방지함. 
+    e.preventDefault();
     console.log(loginInput.value)
-    // console.log("cicked")
-    const value = loginInput.value;
-    if(value === "") {
-        alert("Please write your name")
-    }
-    else if (value.length > 15) {
-        alert("Your name is too long.")
-    }
+    // const value = loginInput.value;
+    // if(value === "") {
+    //     alert("Please write your name")
+    // }
+    // else if (value.length > 15) {
+    //     alert("Your name is too long.")
+    // }
 }
 // function handleInputButton(){
 
 // }
 
-loginButton.addEventListener("click", handleLoginButton)
+loginForm.addEventListener("submit", handleLoginButton)
 // loginInput.addEventListener("")
 
