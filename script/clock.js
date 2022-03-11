62c0cd6f35329622fcb57aaba2fd2807b033461d
LeeGeonwoo22 <kwoo.chris@gmail.com>
2022-03-11 22:01:57 +0900

    clock 추가함

--- script/clock.js
+++ script/clock.js
@@ -1,7 +1,13 @@
 const clock = document.querySelector("h2#clock");
 
-function sayHello(){
-    console.log("hello"); 
+function getClock(){
+    // console.log("hello"); 
+    const date = new Date();
+    const hours = String(date.getHours()).padStart(2, "0");
+    const minutes = String(date.getMinutes()).padStart(2, "0");
+    const seconds = String(date.getSeconds()).padStart(2, "0");
+    clock.innerText = `${hours}:${minutes}:${seconds}`;
+    // clock.innerText = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`
 }
 
-setInterval(sayHello, 5000)
+setInterval(getClock, 1000)
