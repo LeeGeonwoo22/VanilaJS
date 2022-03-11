c3c962d29671ec932564a4edacc03d0fe57b6df4
LeeGeonwoo22 <kwoo.chris@gmail.com>
2022-03-11 18:59:03 +0900

    hidden 숨기기

--- script/app.js
+++ script/app.js
@@ -1,19 +1,21 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = loginForm.querySelector("#login-form input");
-const link = document.querySelector("a")
+const greeting = document .querySelector("#greeting");
+// 위 변수 처럼 중요한 정보를 담은게 아니라 대문자로 작성함.
+const HIDDEN_CLASSNAME = "hidden";
 
 function handleLoginButton(e){
     e.preventDefault();
-    console.log(loginInput.value)
-
-}
-
-function handleLinkClick(e){
-    // 모든 동작을 막게 되므로 alert을 사용하지않음 
-    e.preventDefault();
-    alert("clicked!")
+    // add.("HIDDEN_CLASSNAME") 따옴표 주의
+    loginForm.classList.add(HIDDEN_CLASSNAME)
+    const username = loginInput.value;
+    // greeting.innerText = "Hello " + username;
+    greeting.innerText = `Hello ${username}`;
+    // .remove(HIDDEN_CLASSNAME)
+    greeting.classList.remove(HIDDEN_CLASSNAME)
+    // console.log(username)
 }
 
 loginForm.addEventListener("submit", handleLoginButton)
-link.addEventListener("click", handleLinkClick);
+
 
