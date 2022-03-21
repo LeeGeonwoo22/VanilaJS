2a1dffb42b41a4e53afacf214673873f475eecdc
LeeGeonwoo22 <kwoo.chris@gmail.com>
2022-03-21 15:08:32 +0900

    그림 위에 나오게함

--- script/backGround.js
+++ script/backGround.js
@@ -1,3 +1,4 @@
+const container = document.querySelector(".container")
 const images = [
     "0.jpeg", "1.jpeg", "2.jpeg"
 ]
@@ -8,4 +9,5 @@ const bgImage = document.createElement("img")
 
 bgImage.src = `/styles/img/${chosenImage}`;
 
-document.body.appendChild(bgImage)
+// const div = document.createElement("div")
+container.appendChild(bgImage)
